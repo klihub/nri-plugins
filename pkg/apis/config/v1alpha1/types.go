a45bf2d8dddf3d9ddf5b345e4f84566287702071
Krisztian Litkey <krisztian.litkey@intel.com>
2023-11-15 11:00:36 +0200

    squashme... pkg/apis/config

--- pkg/apis/config/v1alpha1/types.go
+++ pkg/apis/config/v1alpha1/types.go
@@ -15,9 +15,11 @@
 package v1alpha1
 
 import (
+	"encoding/json"
 	"fmt"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/containers/nri-plugins/pkg/apis/config/v1alpha1/instrumentation"
 	"github.com/containers/nri-plugins/pkg/apis/config/v1alpha1/log"
@@ -36,8 +38,9 @@ type TopologyAwareConfig struct {
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	TopologyAware topologyaware.Config `json:"topologyAware"`
-	Common        CommonConfig         `json:"common,omitempty"`
-	Status        ConfigStatus         `json:"status,omitempty"`
+	// +optional
+	Common CommonConfig `json:"common,omitempty"`
+	Status ConfigStatus `json:"status,omitempty"`
 }
 
 // TopologyAwareConfigList represents a list of TopologyAwareConfigs.
@@ -58,8 +61,9 @@ type BalloonsConfig struct {
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	Balloons balloons.Config `json:"balloons"`
-	Common   CommonConfig    `json:"common,omitempty"`
-	Status   ConfigStatus    `json:"status,omitempty"`
+	// +optional
+	Common CommonConfig `json:"common,omitempty"`
+	Status ConfigStatus `json:"status,omitempty"`
 }
 
 // BalloonsConfigList represents a list of BalloonsConfigs.
@@ -80,8 +84,9 @@ type TemplateConfig struct {
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	Template template.Config `json:"template"`
-	Common   CommonConfig    `json:"common,omitempty"`
-	Status   ConfigStatus    `json:"status,omitempty"`
+	// +optional
+	Common CommonConfig `json:"common,omitempty"`
+	Status ConfigStatus `json:"status,omitempty"`
 }
 
 // TemplateConfigList represents a list of TemplateConfigs.
@@ -95,17 +100,19 @@ type TemplateConfigList struct {
 
 type CommonConfig struct {
 	// +optional
-	Control control.Config `json:"control"`
+	Control control.Config `json:"control,omitempty"`
 	// +optional
-	Log log.Config `json:"log"`
+	Log log.Config `json:"log,omitempty"`
 	// +optional
-	Instrumentation instrumentation.Config `json:"instrumentation"`
+	Instrumentation instrumentation.Config `json:"instrumentation,omitempty"`
 }
 
+// ConfigStatus is the per-node status for a configuration resource.
 type ConfigStatus struct {
 	Nodes map[string]NodeStatus `json:"nodes"`
 }
 
+// NodeStatus is the configuration status for a single node.
 type NodeStatus struct {
 	// Status of activating the configuration on this node.
 	// +kubebuilder:validation:Enum=Success;Failure
@@ -114,7 +121,7 @@ type NodeStatus struct {
 	Generation int64 `json:"generation"`
 	// Error can provide further details of a configuration error.
 	Error *string `json:"errors,omitempty"`
-	// Last time of success/failure.
+	// Timestamp of setting this status.
 	Timestamp metav1.Time `json:"timestamp,omitempty"`
 }
 
@@ -123,6 +130,7 @@ const (
 	StatusFailure = metav1.StatusFailure
 )
 
+// NewNodeStatus returns a node status for the given error.
 func NewNodeStatus(err error, generation int64) *NodeStatus {
 	s := &NodeStatus{
 		Generation: generation,
@@ -130,8 +138,8 @@ func NewNodeStatus(err error, generation int64) *NodeStatus {
 	}
 	if err == nil {
 		s.Status = StatusSuccess
-		// TODO(klihub): kludge to 'patch away' any old errors from
-		// lingering. Needs to be fixed properly...
+		// TODO(klihub): 'Patch away' any old errors from lingering. I don't
+		//     know if there is a nicer way of doing this with Patch().
 		e := ""
 		s.Error = &e
 	} else {
@@ -142,18 +150,44 @@ func NewNodeStatus(err error, generation int64) *NodeStatus {
 	return s
 }
 
-// AnyConfig is a generic common interface we expect all configuration
-// (root) types to implement. It provides access to the common metadata
-// bits. It can be used to check if two configuration objects reference
-// the same object instance on the apiserver without having to know the
-// exact specific configuration types of these object. The agent uses it
-// to filter out duplicate config update events when an expired watch is
-// recreated.
+type patchConfig struct {
+	Status patchStatus `json:"status,omitempty"`
+}
+
+type patchStatus struct {
+	Nodes map[string]*NodeStatus `json:"nodes,omitempty"`
+}
+
+func NodeStatusPatch(node string, status *NodeStatus) ([]byte, types.PatchType, error) {
+	cfg := &patchConfig{
+		Status: patchStatus{
+			Nodes: map[string]*NodeStatus{
+				node: status,
+			},
+		},
+	}
+
+	data, err := json.Marshal(cfg)
+	if err != nil {
+		return nil, types.PatchType(""), fmt.Errorf("failed to marshal patch: %v", err)
+	}
+
+	return data, types.MergePatchType, nil
+}
+
+// AnyConfig provides access to metadata of configuration resources.
+// All top level configuration types must implement this interface.
+// Among other things, the agent uses it to filter out unnecessary
+// update notifications.
 // +kubebuilder:object:generate=false
 type AnyConfig interface {
 	GetObjectMeta() *metav1.ObjectMeta
 }
 
+// ResmgrConfig provides access to policy-specific and common
+// configuration data. All resource management configuration
+// types must implement this interface. The resource manager
+// uses it to pass configuration to the policy implementation.
 // +kubebuilder:object:generate=false
 type ResmgrConfig interface {
 	CommonConfig() *CommonConfig
