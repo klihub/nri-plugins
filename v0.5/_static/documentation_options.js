9a09b9415c5eee86c411ebb7e791dd20939c19c9
Github <no-reply@github.com>
2024-04-09 07:12:01 +0000

    Update documentation for v0.5
    Auto-generated from v0.5.1-1-g392cd63 by 'update-gh-pages.sh'

--- v0.5/_static/documentation_options.js
+++ v0.5/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.5.2',
+    VERSION: 'v0.5.1-1-g392cd63',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
