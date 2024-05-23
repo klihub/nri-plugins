1ce1e78071ca2b87ab9e0c656c0b7306ebd01e90
Krisztian Litkey <krisztian.litkey@intel.com>
2024-05-23 11:10:09 +0300

    e2e: use sorted github releases to get latest release.
    Our method of using the github.com/$repo/releases/latest to
    query and determine the latest release of containerd, CRI-O
    or kubernetes is flawed. The semantics of that 'latest' is
    calendar time of release, so it will pick wrong versions if
    a new tagged maintenance release is the newest by calendar
    time.
    
    Ideally we would use the official github API to list all
    tagged releases not marked as a prerelease, and pick the
    latest one by tag. However, without authenticating that
    API is severly rate limited and fails often from behind
    busy proxies. So instead, fetch pages of releases, parse
    them for release URLs, sort them by version and pick the
    latest.
    
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -23,25 +23,40 @@ export cni_plugin=${cni_plugin:-cilium}
 export cni_release=${cni_release:-latest}
 TOPOLOGY_DIR=${TOPOLOGY_DIR:=e2e}
 
-K8S_REPO="https://github.com/kubernetes/kubernetes"
+GH_K8S_REPO="kubernetes/kubernetes"
 export k8s_release=${k8s_release:-"latest"}
 export k8s_version=""
 
 source "$LIB_DIR"/vm.bash
 
+list-github-releases () {
+    local repo="$1" max="${max_releases:-20}" page=1 tags="" more="" sep=""
+    local total=0 cnt=-1
+
+    while [ "$cnt" != 0 -a "$total" -lt "$max" ]; do
+        echo -n "fetching releases for $repo (page $page)..." 1>&2
+        if more=$(wget -q "https://github.com/$repo/releases?page=$page" -O- | \
+                      grep -E '.*/releases/tag/v[0-9]+.[0-9]+.[0-9]+"' | \
+                      sed -E 's|.*/releases/tag/(v[0-9]+.[0-9]+.[0-9]+).*|\1|g')
+        then
+            tags="$tags$sep$more"
+            sep=" "
+            cnt="$(echo $more | wc -w)"
+            total=$((total+cnt))
+            echo " got $cnt more, $total in total..." 1>&2
+            let page=$page+1
+        fi
+    done
+
+    echo "$tags" | tr -s ' ' '\n' | sort -Vr
+}
+
 latest-github-release () {
-    local repo="$1"
-    local latest=$(wget -q -O- $repo/releases/latest | grep '/tree/v[0-9.]*' |
-                       sed -E 's:.*/tree/v([0-9]*\.[0-9]*)(\.[0-9]*).*:\1\2:g' | head -1)
-    if [ -z "$latest" ]; then
-        echo "failed to determine latest release of $repo"
-        exit 1
-    fi
-    echo "$latest"
+    list-github-releases "$1" | head -1
 }
 
 if [ "$k8s_release" = "latest" ]; then
-    if ! k8s_release=$(latest-github-release $K8S_REPO); then
+    if ! k8s_release=$(latest-github-release $GH_K8S_REPO); then
         error "$k8s_release"
     fi
     echo "Latest Kubernetes release: $k8s_release"
@@ -80,11 +95,11 @@ fi
 # from a release tarball with the version given below... unless
 # a source directory is given, which is then expected to contain
 # a compiled version of containerd which we should install.
-CONTAINERD_REPO="https://github.com/containerd/containerd"
+GH_CONTAINERD_REPO="containerd/containerd"
 export containerd_release=${containerd_release:-latest}
 
 if [ "$k8scri" = "containerd" -a "$containerd_release" = "latest" ]; then
-    if ! containerd_release=$(latest-github-release $CONTAINERD_REPO); then
+    if ! containerd_release=$(latest-github-release $GH_CONTAINERD_REPO); then
         error "$containerd_release"
     fi
     echo "Latest containerd release: $containerd_release"
