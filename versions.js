b2e4522564b81ccdee5923b0cbc668b08167a105
Github <no-reply@github.com>
2024-07-02 13:39:40 +0000

    Update documentation for v0.7
    Auto-generated from v0.7.0 by 'update-gh-pages.sh'

--- versions.js
+++ versions.js
@@ -6,5 +6,6 @@ function getVersionsMenuItems() {
     { name: 'v0.2', url: '/nri-plugins/v0.2' },
     { name: 'v0.4', url: '/nri-plugins/v0.4' },
     { name: 'v0.5', url: '/nri-plugins/v0.5' },
+    { name: 'v0.7', url: '/nri-plugins/v0.7' },
   ];
 }
