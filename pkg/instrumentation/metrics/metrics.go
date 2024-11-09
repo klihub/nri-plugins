a44842bf2c3ae137b4320b2165711431fdb5776f
Krisztian Litkey <krisztian.litkey@intel.com>
2024-11-09 23:54:59 +0200

    instrumentation: remove opencensus metrics exporter.
    Remove the old opencensus-based prometheus exporter. Rework
    prometheus exporting using our update metrics registry and
    a promhttp /metrics-handler.
    
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

--- pkg/instrumentation/metrics/metrics.go
+++ pkg/instrumentation/metrics/metrics.go
@@ -15,14 +15,112 @@
 package metrics
 
 import (
-	oc "github.com/containers/nri-plugins/pkg/instrumentation/metrics/opencensus"
+	"fmt"
+	"slices"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/containers/nri-plugins/pkg/http"
+	logger "github.com/containers/nri-plugins/pkg/log"
+	"github.com/containers/nri-plugins/pkg/metrics"
+	_ "github.com/containers/nri-plugins/pkg/metrics/collectors"
+)
+
+type (
+	Option func() error
 )
 
 var (
-	RegisterGatherer     = oc.RegisterGatherer
-	WithExporterDisabled = oc.WithExporterDisabled
-	WithPeriod           = oc.WithPeriod
-	WithServiceName      = oc.WithServiceName
-	Start                = oc.Start
-	Stop                 = oc.Stop
+	disabled     bool
+	namespace    string
+	enabled      []string
+	polled       []string
+	reportPeriod time.Duration
+	mux          *http.ServeMux
+	gatherer     *metrics.Gatherer
+	log          = logger.Get("metrics")
 )
+
+func WithExporterDisabled(v bool) Option {
+	return func() error {
+		disabled = v
+		return nil
+	}
+}
+
+func WithNamespace(v string) Option {
+	return func() error {
+		namespace = v
+		return nil
+	}
+}
+
+func WithReportPeriod(v time.Duration) Option {
+	return func() error {
+		reportPeriod = v
+		return nil
+	}
+}
+
+func WithMetrics(enable []string, poll []string) Option {
+	return func() error {
+		enabled = slices.Clone(enable)
+		polled = slices.Clone(poll)
+		return nil
+	}
+}
+
+func Start(m *http.ServeMux, options ...Option) error {
+	Stop()
+
+	for _, opt := range options {
+		if err := opt(); err != nil {
+			return err
+		}
+	}
+
+	if m == nil {
+		log.Info("no mux provided, metrics exporter disabled")
+		return nil
+	}
+
+	if disabled {
+		log.Info("metrics exporter disabled")
+		return nil
+	}
+
+	log.Info("starting metrics exporter...")
+
+	g, err := metrics.NewGatherer(
+		metrics.WithNamespace("nri"),
+		metrics.WithPollInterval(reportPeriod),
+		metrics.WithMetrics(enabled, polled),
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create metrics gatherer: %v", err)
+	}
+
+	gatherer = g
+
+	handlerOpts := promhttp.HandlerOpts{
+		ErrorLog:      log,
+		ErrorHandling: promhttp.ContinueOnError,
+	}
+	m.Handle("/metrics", promhttp.HandlerFor(g, handlerOpts))
+
+	mux = m
+
+	return nil
+}
+
+func Stop() {
+	if mux == nil {
+		return
+	}
+
+	mux.Unregister("/metrics")
+	mux = nil
+	gatherer.Stop()
+	gatherer = nil
+}
