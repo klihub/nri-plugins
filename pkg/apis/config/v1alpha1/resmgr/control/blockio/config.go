5fc6154c7caa6d3e972d986caf73f6442895a6ae
Krisztian Litkey <krisztian.litkey@intel.com>
2025-06-18 14:47:30 +0300

    block I/O revert out block I/O configuration/setting changes.
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

--- pkg/apis/config/v1alpha1/resmgr/control/blockio/config.go
+++ pkg/apis/config/v1alpha1/resmgr/control/blockio/config.go
@@ -14,21 +14,6 @@
 
 package blockio
 
-import (
-	"slices"
-
-	"github.com/intel/goresctrl/pkg/blockio"
-	grclog "github.com/intel/goresctrl/pkg/log"
-	grcpath "github.com/intel/goresctrl/pkg/path"
-)
-
-var (
-	// Expose goresctrl/blockio functions for configuration via this package.
-	SetPrefix func(string)                      = grcpath.SetPrefix
-	SetLogger func(grclog.Logger)               = blockio.SetLogger
-	SetConfig func(*blockio.Config, bool) error = blockio.SetConfig
-)
-
 // Config provides runtime configuration for class based block I/O
 // prioritization and throttling.
 // +kubebuilder:object:generate=true
@@ -42,47 +27,4 @@ type Config struct {
 	// class is used as its block I/O class, if this is otherwise unset.
 	// +optional
 	UsePodQoSAsDefaultClass bool `json:"usePodQoSAsDefaultClass,omitempty"`
-	// Classes define weights and throttling parameters for sets of devices.
-	// +optional
-	Classes map[string][]DevicesParameters `json:"classes,omitempty"`
-	// Force indicates if the configuration should be forced to goresctrl.
-	Force bool `json:"force,omitempty"`
-}
-
-// +kubebuilder:object:generate=true
-type DevicesParameters struct {
-	Devices           []string `json:"devices,omitempty"`
-	ThrottleReadBps   string   `json:"throttleReadBps,omitempty"`
-	ThrottleWriteBps  string   `json:"throttleWriteBps,omitempty"`
-	ThrottleReadIOPS  string   `json:"throttleReadIOPS,omitempty"`
-	ThrottleWriteIOPS string   `json:"throttleWriteIOPS,omitempty"`
-	Weight            string   `json:"weight,omitempty"`
-}
-
-func (c *Config) ToGoresctrl() (*blockio.Config, bool) {
-	if c == nil {
-		return nil, false
-	}
-
-	out := &blockio.Config{
-		Classes: make(map[string][]blockio.DevicesParameters, len(c.Classes)),
-	}
-
-	for name, params := range c.Classes {
-		out.Classes[name] = make([]blockio.DevicesParameters, len(params))
-		for _, param := range params {
-			out.Classes[name] = append(out.Classes[name],
-				blockio.DevicesParameters{
-					Devices:           slices.Clone(param.Devices),
-					ThrottleReadBps:   param.ThrottleReadBps,
-					ThrottleWriteBps:  param.ThrottleWriteBps,
-					ThrottleReadIOPS:  param.ThrottleReadIOPS,
-					ThrottleWriteIOPS: param.ThrottleWriteIOPS,
-					Weight:            param.Weight,
-				},
-			)
-		}
-	}
-
-	return out, c.Force
 }
