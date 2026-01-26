843ac437bdced1da6c19bb34c08a8eb2d3e3d7af
Github <no-reply@github.com>
2026-01-26 14:21:43 +0000

    Update documentation for v0.11
    Auto-generated from v0.11.0 by 'update-gh-pages.sh'

--- releases/releases.js
+++ releases/releases.js
@@ -2,6 +2,7 @@ function getReleaseListItems() {
   return [
     { name: 'v0.10.0', url: 'v0.10.0' },
     { name: 'v0.10.1', url: 'v0.10.1' },
+    { name: 'v0.11.0', url: 'v0.11.0' },
     { name: 'v0.4.1', url: 'v0.4.1' },
     { name: 'v0.5.0', url: 'v0.5.0' },
     { name: 'v0.5.1', url: 'v0.5.1' },
